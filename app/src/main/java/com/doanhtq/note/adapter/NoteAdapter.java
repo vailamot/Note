7d2dafc99e390b510a8bb1917a879939eccc70c2
doanhtq <doanhtqvlm@gmail.com>
2022-05-13 11:39:05 +0700

    read provider

--- app/src/main/java/com/doanhtq/note/adapter/NoteAdapter.java
+++ app/src/main/java/com/doanhtq/note/adapter/NoteAdapter.java
@@ -1,4 +1,4 @@
-package com.doanhtq.note;
+package com.doanhtq.note.adapter;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -9,6 +9,9 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+
+import com.doanhtq.note.Note;
+import com.doanhtq.note.R;
 
 import java.util.LinkedList;
 
